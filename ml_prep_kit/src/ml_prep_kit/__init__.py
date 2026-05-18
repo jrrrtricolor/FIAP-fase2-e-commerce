803c1115cc57e838a034b09b87481d5be220332f
“Cassio <jrrrtricolor@gmail.com>
2026-05-18 20:07:13 +0100

    feat: adiciona utilitários visuais ao ml_prep_kit
    Cria a classe VisualizationReporter para centralizar gráficos simples usados nos notebooks, como rankings, comparações, linha do tempo e matriz de calor.
    
    Adiciona funções utilitárias para formatação de moeda e percentual, reduzindo duplicação nos notebooks.
    
    Exporta os novos recursos no pacote para permitir reutilização em outros projetos de análise.

--- ml_prep_kit/src/ml_prep_kit/__init__.py
+++ ml_prep_kit/src/ml_prep_kit/__init__.py
@@ -4,10 +4,15 @@ from ml_prep_kit.csv_data_loader import CSVDataLoader
 from ml_prep_kit.data_validator import DataValidator
 from ml_prep_kit.feature_preprocessor import FeaturePreprocessor
 from ml_prep_kit.sqlite_dataframe_store import SQLiteDataFrameStore
+from ml_prep_kit.utils import format_currency, format_percent
+from ml_prep_kit.visualization_reporter import VisualizationReporter
 
 __all__ = [
     "CSVDataLoader",
     "DataValidator",
     "FeaturePreprocessor",
     "SQLiteDataFrameStore",
+    "VisualizationReporter",
+    "format_currency",
+    "format_percent",
 ]
